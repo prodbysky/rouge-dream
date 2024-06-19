c27a162f10ed275c745cd40ff97eca59e456b231
prodbysky <kyrbynyt@gmail.com>
2024-06-19 11:44:51 +0300

    Add utility function.

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,3 +13,12 @@ float CenterMouseAngle(const Vector2& point, const Camera2D& camera) {
         angle += 360;
     return angle - 45;
 }
+
+// NOTE: Angle is in degress
+Vector2 Vector2MoveToAngle(const Vector2& origin, const float& angle,
+                           const Vector2& speed) {
+    Vector2 rotation_angle =
+        Vector2Normalize(Vector2Rotate({1, 1}, DEG2RAD * angle));
+
+    return Vector2Add(origin, Vector2Multiply(speed, rotation_angle));
+}
--- src/utils.h
+++ src/utils.h
@@ -3,3 +3,5 @@
 #include <raylib.h>
 
 float CenterMouseAngle(const Vector2& point, const Camera2D& camera);
+Vector2 Vector2MoveToAngle(const Vector2& origin, const float& angle,
+                           const Vector2& speed);
